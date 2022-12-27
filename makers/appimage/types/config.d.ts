209594ca16b9461ac2bcf3b7d8c335fdfc6d6f96
SpacingBat3 <git@spacingbat3.anonaddy.com>
2022-12-27 01:04:56 +0100

    maker-appimage: Update `config` definition file.
    • Use string templates for some of FreeDesktop types.
    • Update JSDoc comments.

--- makers/appimage/types/config.d.ts
+++ makers/appimage/types/config.d.ts
@@ -1,20 +1,41 @@
 interface MakerAppImageConfigOptions {
     /**
-     * Name of the executable, prefferably in lowercase letters. Should not have
-     * spaces or any special characters.
+     * Name of the package to be used as folder name, `X-AppImage-Name` and
+     * executable etc. It should contain only lowercase letters and hypens.
+     * It should also start from a letter and contain at least one character.
+     * 
+     * If above requirements aren't met, maker will approach to sanitize the
+     * string to match the required format. This process might fail if
+     * the string is nowhere close to match the requirements, so the one
+     * shouldn't depend on the sanitizer when setting the `name` property on
+     * their own.
+     * 
+     * Defaults to `packageJSON.name`
      */
     name?: string,
-    /** Human-friendly name of the application. */
+    /**
+     * Human-friendly name of the application.
+     * 
+     * Defaults to `packageJSON.productName`
+     */
     productName?: string,
-    /** Generic name of the application used in `.desktop` file. */
+    /**
+     * Generic name of the application used in `.desktop` file.
+     */
     genericName?: string,
-    /** Path to icon to use for AppImage */
+    /**
+     * Path to icon to use for the AppImage.
+     */
     icon?: string,
-    /** List of dekstop file categories to append. */
+    /**
+     * List of dekstop file categories to append.
+     */
     categories?: (
         FreeDesktopCategories["main"] | FreeDesktopCategories["additional"]
     )[],
-    /** Actions to be used within a generated desktop file */
+    /**
+     * Actions to be used within a generated desktop file.
+     */
     actions?: Record<string, Partial<Record<string,string|null>> & {
         /** Action's user-friendly name */
         Name: string,
@@ -23,21 +44,35 @@ interface MakerAppImageConfigOptions {
         /** A command to execute on a desktop file action */
         Exec?: string|null
     }>
-    /** Path to desktop file to be used instead of generating a new one. */
+    /**
+     * Path to desktop file to be used instead of generating a new one.
+     */
     desktopFile?: string,
-    /** Whenever calculate and embed MD5 digest in the runtime. **Default**: `true` */
+    /**
+     * Whenever calculate and embed MD5 digest in the runtime.
+     * 
+     * Defaults to `true`
+     */
     //digestMd5?: boolean,
-    /** Use given compressor for SquashFS filesystem. Defaults to `mksquasfs` binary defaults. */
+    /**
+     * Use given compressor for SquashFS filesystem.
+     * 
+     * Defaults to `mksquasfs` binary defaults (usually `gzip`).
+     */
     compressor?: "xz"|"gzip"|"lz4"|"lzo"|"zstd"|"lzma"
     /**
      * GitHub Release of `AppImage/AppImageKit` from which this maker should
-     * get the runtime and AppRun executable. By default it is `13`.
+     * get the runtime and AppRun executable. 
+     * 
+     * Defaults to `13`.
      */
     AppImageKitRelease?: number | "continuous"
 }
 
-/** AppImage maker configuration options */
 export interface MakerAppImageConfig {
+    /**
+     * AppImage maker configuration options.
+     */
     options?: MakerAppImageConfigOptions
 }
 
@@ -69,28 +104,27 @@ export interface MakerAppImageConfig {
         "RevisionControl" | "Translation" | "Calendar" | "ContactManagement" |
         "Database" | "Dictionary" | "Chart" | "Email" | "Finance" |
         "FlowChart" | "PDA" | "ProjectManagement" | "Presentation" |
-        "Spreadsheet" | "WordProcessor" | "2DGraphics" | "VectorGraphics" |
-        "RasterGraphics" | "3DGraphics" | "Scanning" | "OCR" | "Photography" |
-        "Publishing" | "Viewer" | "TextTools" | "DesktopSettings" |
-        "HardwareSettings" | "Printing" | "PackageManager" | "Dialup" |
-        "InstantMessaging" | "Chat" | "IRCClient" | "Feed" | "FileTransfer" |
-        "HamRadio" | "News" | "P2P" | "RemoteAccess" | "Telephony" |
-        "TelephonyTools" | "VideoConference" | "WebBrowser" | "WebDevelopment" |
-        "Midi" | "Mixer" | "Sequencer" | "Tuner" | "TV" | "AudioVideoEditing" |
-        "Player" | "Recorder" | "DiscBurning" | "ActionGame" | "AdventureGame" |
-        "ArcadeGame" | "BoardGame" | "BlocksGame" | "CardGame" | "KidsGame" |
-        "LogicGame" | "RolePlaying" | "Shooter" | "Simulation" | "SportsGame" |
-        "StrategyGame" | "Art" | "Construction" | "Music" | "Languages" |
-        "ArtificialIntelligence" | "Astronomy" | "Biology" | "Chemistry" |
-        "ComputerScience" | "DataVisualization" | "Economy" | "Electricity" |
-        "Geography" | "Geology" | "Geoscience" | "History" | "Humanities" |
-        "ImageProcessing" | "Literature" | "Maps" | "Math" |
+        "Spreadsheet" | "WordProcessor" | "Scanning" | "OCR" | "Photography" |
+        `${"2D"|"3D"|"Vector"|"Raster"}Graphics` | "Publishing" | "Viewer" |
+        `Text${"Tools"|"Editor"}` | "DesktopSettings" | "HardwareSettings" |
+        "Printing" | "PackageManager" | "Dialup" | "InstantMessaging" | "Chat" |
+        "IRCClient" | "Feed" | "FileTransfer" | "HamRadio" | "News" | "P2P" |
+        "RemoteAccess" | `Telephony${""|"Tools"}` | "VideoConference" |
+        `Web${"Browser"|"Development"}` | "Midi" | "Mixer" | "Sequencer" |
+        "Tuner" | "TV" | "AudioVideoEditing" | "Player" | "Recorder" |
+        "DiscBurning"  | "RolePlaying" | "Shooter" | `${"Action" | "Adventure" |
+        "Arcade" | "Board" | "Blocs" | "Card" | "Kids" | "Logic" | "Sports" |
+        "Strategy"}Game` | "Simulation" | "Art" | "Construction" | "Music" |
+        "Languages" | "ArtificialIntelligence" | "Astronomy" | "Biology" |
+        "Chemistry" | "ComputerScience" | "DataVisualization" | "Economy" |
+        "Electricity" | `Geo${"graphy"|"logy"|"science"}` | "History" |
+        "Humanities" | "ImageProcessing" | "Literature" | "Maps" | "Math" |
         "NumericalAnalysis" | "MedicalSoftware" | "Physics" | "Robotics" |
         "Spirituality" |"Sports" |"ParallelComputing" | "Amusement" |
-        "Archiving" | "Compression" | "Electronics" | "Emulator" |
-        "Engineering" | "FileTools" | "FileManager" | "TerminalEmulator" |
-        "Filesystem" | "Monitor" | "Security" | "Accessibility" | "Calculator" |
-        "Clock" | "TextEditor" | "Documentation" | "Adult" | "Core" | "KDE" |
-        "GNOME" | "XFCE" | "GTK" | "Qt" | "Motif" | "Java" | "ConsoleOnly"
+        "Archiving" | "Compression" | "Electronics" | "Engineering" |
+        `File${"Tools"|"Manager"|"system"}` | "Monitor" | "Security" |
+        `${""|"Terminal"}Emulator` | "Accessibility" | "Calculator" | "Clock" |
+        "Documentation" | "Adult" | "Core" | "KDE" | "GNOME" | "XFCE" | "GTK" |
+        "Qt" | "Motif" | "Java" | "ConsoleOnly"
     );
 }
