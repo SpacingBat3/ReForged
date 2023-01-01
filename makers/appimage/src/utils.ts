878c715bd4125048670895b296c16ca6b8ba9294
SpacingBat3 <git@spacingbat3.anonaddy.com>
2023-01-01 15:33:06 +0100

    maker-appimage: Improve `name` sanitization.
    • Allow to replace patterns in sanitization function.
    • Fix issues with unsanitized string detection.

--- makers/appimage/src/utils.ts
+++ makers/appimage/src/utils.ts
@@ -336,19 +336,24 @@ export function setChecksum(runtime:ArrayBuffer|Buffer,squashfs:Buffer): Buffer 
 
 type nullString<T> = T extends null|undefined ? T : string;
 
-export function sanitizeName<T>(name:T):nullString<T>  {
+export function sanitizeName<T>(name:T, valid="a-z0-9", replacement="-"): nullString<T> {
   if(name === null || name === undefined)
     return name as nullString<T>;
+  valid = valid.replaceAll(/([\]^\\])/g,"\\$1");
   let stringName:string;
+  const regexp = {
+    valid: new RegExp(`[${valid}]`),
+    invalid: new RegExp(`[^${valid}${replacement.replaceAll("]","\\]")}]`,"g")
+  }
   if(typeof name !== "string")
     stringName = String(name);
   else
     stringName = name;
-  if(/^[^a-z0-9-]*$/.test(stringName))
+  if(regexp.invalid.test(stringName)||stringName.startsWith(replacement))
     stringName = stringName
       .toLowerCase()
-      .slice(stringName.search(/[a-z0-9]/))
-      .replaceAll(/[^a-z0-9-]/g,"-")
+      .slice(stringName.search(regexp.valid))
+      .replaceAll(regexp.invalid,replacement)
   if(stringName.length === 0)
     throw new Error("Parameter 'name' is not sanitizable!");
   return stringName as nullString<T>;
