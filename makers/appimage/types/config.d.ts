1e1b21eb119d8c5eb3e09419fcb4d3310d4fb832
SpacingBat3 <git@spacingbat3.anonaddy.com>
2025-02-24 17:44:30 +0100

    maker-types: Share common types between makers.
    Introduce module that will be used to create a common configuration
    structure between makers.

--- makers/appimage/types/config.d.ts
+++ makers/appimage/types/config.d.ts
@@ -1,75 +1,6 @@
-export interface MakerAppImageConfigOptions {
-    /**
-     * Name of the package (used as folder name, `X-AppImage-Name` etc).
-     * It should contain only lowercase letters and hyphens. It should also start
-     * from a letter and contain at least one character.
-     *
-     * If above requirements aren't met, maker will approach to sanitize the
-     * string to match the required format. This process might fail if
-     * the string is nowhere close to match the requirements, so the one
-     * shouldn't depend on the sanitizer when setting the `name` property on
-     * their own.
-     *
-     * Defaults to sanitized `packageJSON.name`.
-     *
-     * @since v1.0.0
-     *
-     */
-    name?: string,
-    /**
-     * Name of the executable to put into the `Exec` field of generated
-     * `.desktop` file.
-     *
-     * Defaults to `options.name`.
-     */
-    bin?: string,
-    /**
-     * Human-friendly name of the application.
-     *
-     * Defaults to `packageJSON.productName`.
-     *
-     * @since v1.0.0
-     */
-    productName?: string,
-    /**
-     * Generic name of the application used in `.desktop` file.
-     *
-     * @since v1.0.0
-     */
-    genericName?: string,
-    /**
-     * Path to icon to use for the AppImage.
-     *
-     * @since v1.0.0
-     */
-    icon?: string,
-    /**
-     * List of desktop file categories to append.
-     *
-     * @since v1.0.0
-     */
-    categories?: (
-        FreeDesktopCategories["main"] | FreeDesktopCategories["additional"]
-    )[],
-    /**
-     * Actions to be used within a generated desktop file.
-     *
-     * @since v2.1.0
-     */
-    actions?: Record<string, Partial<Record<string,string|null>> & {
-        /** Action's user-friendly name */
-        Name: string,
-        /** A path to action icon */
-        Icon?: string|null,
-        /** A command to execute on a desktop file action */
-        Exec?: string|null
-    }>
-    /**
-     * Path to desktop file to be used instead of generating a new one.
-     *
-     * @since v2.1.0
-     */
-    desktopFile?: string,
+import {MakerConfig, MakerUnixOptions} from "@reforged/maker-types"
+
+interface MakerAppImageConfigOptions extends MakerUnixOptions {
     /**
      * Whenever calculate and embed MD5 digest in the runtime.
      *
@@ -119,68 +50,7 @@ export interface MakerAppImageConfigOptions {
      */
     type2runtime?: boolean
 }
-
-export interface MakerAppImageConfig {
-    /**
-     * AppImage maker configuration options.
-     *
-     * @since v1.0.0
-     */
-    options?: MakerAppImageConfigOptions
-}
-
-/**
- * FreeDesktop software categories, grouped as in FreeDesktop specification.
- *
- * @privateRemarks
- *
- * Generated using DevTools with:
- * ```js
- * const out = [];
- * for(const td of document.querySelectorAll("table.informaltable>tbody>tr>td:first-child"))
- *     out.push('"'+td.innerText+'"');
- * console.log(out.join(' | '));
- * ```
- */
- export interface FreeDesktopCategories {
-    /** Categories that every desktop confirming environment **must** support. */
-    main: (
-        "AudioVideo" | "Audio" | "Video" | "Development" | "Education" |
-        "Game" | "Graphics" | "Network" | "Office" | "Science" | "Settings" |
-        "System" | "Utility"
-    );
-    /**
-     * Categories that provide more fine grained information about the
-     * application.
-     */
-    additional: (
-        "Building" | "Debugger" | "IDE" | "GUIDesigner" | "Profiling" |
-        "RevisionControl" | "Translation" | "Calendar" | "Database" |
-        "Dictionary" | "Email" | "Finance" | `${"Flow"|""}Chart` | "PDA" |
-        "Presentation" | "Spreadsheet" | "WordProcessor" | "Scanning" | "OCR" |
-        "Photography" | `${"Contact"|"Project"}Management` |
-        `${"2D"|"3D"|"Vector"|"Raster"}Graphics` | "Publishing" | "Viewer" |
-        `Text${"Tools"|"Editor"}` | "DesktopSettings" | "HardwareSettings" |
-        "Printing" | "PackageManager" | "Dialup" | "InstantMessaging" | "Chat" |
-        "IRCClient" | "Feed" | "FileTransfer" | "HamRadio" | "News" | "P2P" |
-        "RemoteAccess" | `Telephony${""|"Tools"}` | "VideoConference" |
-        `Web${"Browser"|"Development"}` | "Midi" | "Mixer" | "Sequencer" |
-        "Tuner" | "TV" | "AudioVideoEditing" | "Player" | "Recorder" |
-        "DiscBurning"  | "RolePlaying" | "Shooter" | `${"Action" | "Adventure" |
-        "Arcade" | "Board" | "Blocs" | "Card" | "Kids" | "Logic" | "Sports" |
-        "Strategy"}Game` | "Simulation" | "Art" | "Construction" | "Music" |
-        "Languages" | "ArtificialIntelligence" | "Astronomy" | "Biology" |
-        "Chemistry" | "ComputerScience" | "DataVisualization" | "Economy" |
-        "Electricity" | `Geo${"graphy"|"logy"|"science"}` | "History" |
-        "Humanities" | "ImageProcessing" | "Literature" | "Maps" | "Math" |
-        "NumericalAnalysis" | "MedicalSoftware" | "Physics" | "Robotics" |
-        "Spirituality" |"Sports" |"ParallelComputing" | "Amusement" |
-        "Archiving" | "Compression" | "Electronics" | "Engineering" |
-        `File${"Tools"|"Manager"|"system"}` | "Monitor" | "Security" |
-        `${""|"Terminal"}Emulator` | "Accessibility" | "Calculator" | "Clock" |
-        "Documentation" | "Adult" | "Core" | "KDE" | "GNOME" | "XFCE" | "GTK" |
-        "Qt" | "Motif" | "Java" | "ConsoleOnly"
-    );
-}
+interface MakerAppImageConfig extends MakerConfig<MakerAppImageConfigOptions> {}
 
 export default MakerAppImageConfig;
+export { MakerAppImageConfig, MakerAppImageConfigOptions }
