df288b26773a981f5d9c00bc2569a7626a80f606
SpacingBat3 <git@spacingbat3.anonaddy.com>
2025-08-30 01:15:06 +0200

    project: Add custom footer to Typedoc.

--- .config/typedoc.config.mjs
+++ .config/typedoc.config.mjs
@@ -1,5 +1,6 @@
 import {footnote} from "@mdit/plugin-footnote"
 import {tasklist} from "@mdit/plugin-tasklist";
+import typedoc from "typedoc/package.json" with { type: "json" }
 
 /** @type {import("typedoc").TypeDocOptions}*/
 export default {
@@ -24,6 +25,13 @@ export default {
   name: "ReForged",
   theme: "default",
   out: "../docs/typedoc",
+  customFooterHtml: [
+    'Copyright © 2025 Dawid Papiewski "<a href="https://github.com/SpacingBat3">SpacingBat3</a>"',
+    [
+      'Made with <span title="love">❤️</span> in <a href="https://www.openstreetmap.org/relation/49715" title="Poland">🇵🇱️</a>',
+      `with a help of <a href="${typedoc.homepage}">Typedoc</a> <code>v${typedoc.version}</code>`
+    ].join(', ')
+  ].map(str => `<p align="center">${str}</p>`).join(""),
   /** @param {import("markdown-it").default} parser  */
   markdownItLoader(parser) {
     parser
