36b38f52ac70618745ef8eb18d7e708750d1d3e0
SpacingBat3 <git@spacingbat3.anonaddy.com>
2025-08-29 21:17:22 +0200

    project: Add missing Typedoc markdown parsing.
    For a long time, I used Typedoc with default markdown parsing
    and since v0.24.0 Typedoc has actually switched from `marked` to
    `markdown-it`, which I believe is far from compatible with GFM without
    plugins. So there's no better time than today to add some plugins
    to support some formatting I use in my markdown files.

---
+++ .config/typedoc.config.mjs
@@ -0,0 +1,32 @@
+import {footnote} from "@mdit/plugin-footnote"
+import {tasklist} from "@mdit/plugin-tasklist";
+
+/** @type {import("typedoc").TypeDocOptions}*/
+export default {
+  githubPages: true,
+  hideGenerator: true,
+  entryPointStrategy: "packages",
+  entryPoints: [
+    "../makers/appimage",
+    "../shared/maker-types"
+  ],
+  commentStyle: "jsdoc",
+  visibilityFilters: {},
+  lightHighlightTheme: "github-light",
+  darkHighlightTheme: "github-dark",
+  gitRevision: "master",
+  excludeNotDocumented: false,
+  excludeInternal: true,
+  excludeExternals: false,
+  excludePrivate: true,
+  readme: "../docs/Readme.md",
+  name: "ReForged",
+  theme: "default",
+  out: "../docs/typedoc",
+  /** @param {import("markdown-it").default} parser  */
+  markdownItLoader(parser) {
+    parser
+      .use(footnote)
+      .use(tasklist);
+  }
+}
